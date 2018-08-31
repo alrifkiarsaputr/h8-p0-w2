f660dba6e488dcf2a07b1dfdf942934917a163a4
alrifkiarsaputr <putraalrifki@gmail.com>
2018-08-31 22:30:42 +0700

    tambah Exercises 5

---
+++ Exercises_5.js
@@ -0,0 +1,68 @@
+//soal nomor 1
+console.log('Soal nomor 1')
+
+var word='JavaScript'
+var second='is'
+var third='awesome'
+var fourth='and'
+var fifth='I'
+var sixth='love'
+var seventh='it!'
+
+console.log(word+' '+second+' '+third+' '+fourth+' '+fifth+' '+sixth+' '+seventh)
+console.log('---')
+
+//soal nomor 2
+console.log('Soal nomor 2')
+var word='wow JavaScript is so cool'
+var exampleFirstWord=word[0]+word[1]+word[2]
+var secondWord=word[4]+word[5]+word[6]+word[7]+word[8]+word[9]+word[10]+word[11]+word[12]+word[13]
+var thirdWord=word[15]+word[16]
+var fourthWord=word[18]+word[19]
+var fifthWord=word[21]+word[22]+word[23]+word[24]
+
+console.log('First Word: '+exampleFirstWord)
+console.log('Second Word: '+secondWord)
+console.log('Third Word: '+thirdWord)
+console.log('Fourth Word: '+fourthWord)
+console.log('Fifth Word: '+fifthWord)
+console.log('---')
+console.log('Soal nomor 3')
+
+//soal nomor 3
+var word3='wow JavaScript is so cool'
+var exampleFirstWord3=word3.substring(0,3)
+var secondWord3=word3.substring(4,14)
+var thirdWord3=word3.substring(15,17)
+var fourthWord3=word3.substring(18,20)
+var fifthWord3=word3.substring(21,25)
+
+console.log('First Word: '+exampleFirstWord3)
+console.log('Second Word: '+secondWord3)
+console.log('Third Word: '+thirdWord3)
+console.log('Fourth Word: '+fourthWord3)
+console.log('Fifth Word: '+fifthWord3)
+console.log('---')
+console.log('Soal nomor 4')
+
+//Soal nomor 4
+var w4='wow JavaScript is so cool'
+var fw4=w4.substring(0,3)
+var sw4=w4.substring(4,14)
+var tw4=w4.substring(15,17)
+var fow4=w4.substring(18,20)
+var ffw4=w4.substring(21,25)
+
+var fwl=fw4.length
+var swl=sw4.length
+var twl=tw4.length
+var fowl=fow4.length
+var ffwl=ffw4.length
+
+console.log('First Word: '+fw4+', with length: '+fwl)
+console.log('Second Word: '+sw4+', with length: '+swl)
+console.log('Third Word: '+tw4+', with length: '+twl)
+console.log('Fourth Word: '+fow4+', with length: '+fowl)
+console.log('Fifth Word: '+ffw4+', with length: '+ffwl)
+
+

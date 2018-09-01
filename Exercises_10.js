e1594ed0a9625dfc625bf05580e59944cbe4687a
alrifkiarsaputr <putraalrifki@gmail.com>
2018-09-01 17:15:07 +0700

    tambah Exercises 10

---
+++ Exercises_10.js
@@ -0,0 +1,20 @@
+var angka1=''
+var angka2=''
+
+function bandingkanAngka(angka1,angka2){
+  if(angka2>angka1){
+    return true
+  }
+  else if(angka2<angka1){
+    return false
+  }
+  else{
+    return '-1'
+  }
+}
+//test cases
+console.log(bandingkanAngka(5,8))
+console.log(bandingkanAngka(5,3))
+console.log(bandingkanAngka(4,4))
+console.log(bandingkanAngka(3,3))
+console.log(bandingkanAngka(17,2))
